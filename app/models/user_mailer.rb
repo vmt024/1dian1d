01fd1fa964ab540d219a9d226788cf5f9948051b
peter zhang <vmt024@gmail.com>
2011-06-11 23:09:09 +0000

    include all the images in the email

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -5,6 +5,10 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @url = "http://www.1dian1di.com/"
+    attachments.inline['ribbon.jpg'] = File.read(Rails.public_path + '/images/ribbon.jpg')
+    attachments.inline['header-bg.jpg'] = File.read(Rails.public_path + '/images/header-bg.jpg')
+    attachments.inline['line-break-2.jpg'] = File.read(Rails.public_path + '/images/line-break-2.jpg')
+    attachments.inline['date-bg.jpg'] = File.read(Rails.public_path + '/images/date-bg.jpg')
     mail(:to => user.email,
          :subject => "会员注册成功，一点一滴欢迎您的加入",
          :date => Date.today,
@@ -14,6 +18,10 @@ class UserMailer < ActionMailer::Base
   def password_recover(user)
     @user = user
     @url = "http://www.1dian1di.com/"
+    attachments.inline['ribbon.jpg'] = File.read(Rails.public_path + '/images/ribbon.jpg')
+    attachments.inline['header-bg.jpg'] = File.read(Rails.public_path + '/images/header-bg.jpg')
+    attachments.inline['line-break-2.jpg'] = File.read(Rails.public_path + '/images/line-break-2.jpg')
+    attachments.inline['date-bg.jpg'] = File.read(Rails.public_path + '/images/date-bg.jpg')
     mail(:to => user.email,
          :subject => "一点一滴已经为您更新了密码",
          :date => Date.today,
