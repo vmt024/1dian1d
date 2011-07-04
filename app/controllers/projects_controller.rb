495da0b1acf4cee346214f54fe315e72eb14fff0
Peter Zhang <vmt024@gmail.com>
2011-07-04 23:19:50 +1200

    bug fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -210,7 +210,7 @@ class ProjectsController < ApplicationController
   def not_support_this_project
       support = UserProject.where("project_id = ? and user_id = ?", params[:project_id],session[:current_user_id]).first
       support.destroy unless support.blank? 
-      session[:followed_progress].delete(params[:project_id]) if session[:followed_progress].include?(params[:project_id])
+      session[:followed_progress].delete(params[:project_id]) unless session[:followed_progress].blank?
       @project = Project.find(params[:project_id])
       @project.number_of_supporters -= 1
       @project.save
