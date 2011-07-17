f1ee000da939fabd3e2fb51edeff95221bffe429
Peter Zhang <vmt024@gmail.com>
2011-07-17 12:57:19 +1200

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,6 +66,7 @@ class User < ActiveRecord::Base
     def closed_projects 
       list = []
       projects = self.my_projects.where("complete_time > ? and success_yn is null",self.last_login_time)
+      logger.error("closed_project: debug")
       projects.collect{|p| list << p.id.to_i unless list.include?(p.id.to_i)}
       return list
     end
